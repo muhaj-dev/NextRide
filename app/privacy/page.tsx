83a9e4cd6d65db5736259bfb397d3b0cb2d8e50f
muhammod1 <muhammodholawale@gmail.com>
2023-11-30 22:14:23 +0100

    google sheet update

---
+++ app/privacy/page.tsx
@@ -0,0 +1,194 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="py-32 mb-40 max-width text-[1.1rem]">
+      <h1 className="font-bold text-[3rem] text-center mb-6">
+        {" "}
+        PRIVACY NOTICE INFORMATION
+      </h1>
+      The Nextride Privacy Notice describes the information we collect, how it
+      is used and shared, and your choices regarding this information. This
+      privacy notice lets you know how we promise to look after your personal
+      data. This notice tells you things like what information we use to provide
+      you with our bundle of transportation services, how we use your
+      information to keep you safe, and the choices and controls available to
+      you. This notice also tells you about your privacy rights and how data
+      protection rules work to protect everyone. This notice applies to any
+      users of the services of Nextride or its affiliates anywhere in the world,
+      and to anyone else who contacts Nextride or otherwise submits information
+      to Nextride, unless stated in the Privacy Notice. This privacy policy
+      shall be in accordance to the provision of the Nigerian Data Protection
+      Regulation. The term "us" or "we" refers to the owner of the Nextride app,
+      Nextride Technology is a private limited company, registered under the
+      Companies and Allied Matters Act (CAMA), 2020. The term “you” or “your”
+      refers to the passenger or rider who requests, hires and/or receives a
+      transportation service through their Nextride app account.
+      <h1 className="font-bold text-[2rem] text-center mt-6">
+        PRIVACY OVERVIEW
+      </h1>
+      <h1 className="font-bold text-[1.5rem] mt-4">
+        {" "}
+        PURPOSE OF THE PROCESSING
+      </h1>
+      <ul className="list-decimal my-3 pl-8 flex flex-col gap-5">
+        <li>
+          We process your personal data so we can provide you with the best and
+          most suitable of our services: Through our app, you’re able to hire a
+          vehicle by connecting to a driver that’s available for pickup: We
+          collect and process personal data for the purpose of connecting
+          passengers with drivers so you can be picked up and dropped off; or to
+          show you where our nearest available hire-vehicles are located
+        </li>
+        <li>
+          To identify you: in order to provide the best of our services to you,
+          we must be able to identify you as a living person. Hence personal
+          data such as your name, contact information, address, geolocation and
+          so on is collected. Then such data will be processed and used by us in
+          order to identify you and in the same vein understand your needs that
+          are related to the services we provide on the Nextride app.
+        </li>
+        <li>
+          Our priority is to make sure you arrive at your intended destination:
+          We display geolocation data and the phone number of passengers to
+          drivers when using our app to enable efficient pick-up and drop-off,
+          and make the journey and route transparent. This data is collected
+          only when the Nextride app is log on to and put to use by you. The
+          collection of geolocation data stops after closing the Nextride app.
+        </li>
+        <li>
+          We make sure your ride is the quickest, cheapest and most convenient
+          to you: We collect data on the routes taken by our passengers and
+          riders to analyse geographic coverage. This lets us do things like
+          improve recommendations to the drivers about the most efficient
+          routes, and helps us make sure that our services are effectively
+          maximized by you.
+        </li>
+        <li>
+          We maintain and promote standards: We collect data about journey
+          statuses, time and passenger ratings from driver feedback to encourage
+          user safety, promote compliance with our terms and conditions, and
+          make sure we’re providing a quality and enjoyable service to everyone.
+          Customer support data and correspondence is collected for the purposes
+          of feedback, and resolving disputes and service quality issues.
+        </li>
+        <li>
+          To update you about any new development in our services: data provided
+          by you are used to send push notifications to your mobile phones,
+          which could be a new or improved service we now offer, thereby making
+          it available to you in real time. Basically push notifications sent
+          are all in your favor in order to understand your needs and come up
+          with a reliable solution.
+        </li>
+        <li>
+          Through our authentication and verification checks, we are able to
+          keep your accounts safe: We request for your contact details whenever
+          you open an account on our app and then send verification codes to the
+          contact details you provided us with in order to ensure that we’re
+          dealing with you directly and prevent any third party from using your
+          information on our app.
+        </li>
+        <li>
+          To improve the quality, variety, and availability of services
+          offered/provided; data collected from our users are processed to
+          determine how much of each of our services is frequently requested for
+          and used. This will help us improve on the less requested ones, also
+          make often readily available the more requested ones and to provide
+          new services based on the request of our users to expand our
+          operational basis in your favor.
+        </li>
+      </ul>
+      <br />
+      <h1 className="font-bold text-[1.5rem] my-8">
+        PROCESSING OF PERSONAL DATA FOR OTHER PURPOSES
+      </h1>
+      We might process your Personal Data for additional purposes that are not
+      mentioned here, but are compatible with the original purpose for which the
+      data was gathered. To do this, we will ensure that:
+      <br />
+      <br />
+      *the link between purposes, context and nature of Personal Data is
+      suitable for further Processing;
+      <div className="mt-3" />
+      *the further Processing would not harm your interests and
+      <div className="mt-3" />
+      *there would be appropriate safeguard for Processing.
+      <div className="mt-3" />
+      *we will inform you of any further Processing and purposes.
+      <h1 className="font-bold text-[1.5rem] my-8">
+        WHO ELSE CAN ACCESS YOUR PERSONAL DATA?
+      </h1>
+      We do not share your Personal Data with strangers. Personal Data about you
+      is in some cases provided to our trusted partners in order to either make
+      providing the service to you possible or to enhance your customer
+      experience, but In some circumstances, where we are legally obliged to
+      share information with external recipients, for example, under a Court
+      Order or where we cooperate with a data protection supervisory authority
+      in handling complaints or investigations we will have no other choice but
+      to comply with the demands of the law officials.We can also respond to
+      requests, such as those from law enforcement agencies, when we have a
+      good-faith belief that the response is required by law in that
+      jurisdiction or furthers a public interest task, affects users in that
+      jurisdiction, and is consistent with internationally recognized standards.
+      In any scenario, we’ll always satisfy ourselves that we have a lawful
+      basis on which to share the information, and we’ll make sure that we
+      document our decision.
+      <h1 className="font-bold text-[1.5rem] my-8">ADVERTISING</h1>
+      This type of service allows User Data to be utilized for advertising
+      communication purposes displayed in the form of banners and other
+      advertisements on this Website, possibly based on User interests.
+      <div className="mt-3" />
+      This does not mean that all Personal Data are used for this purpose.
+      Information and conditions of use are shown below.
+      <div className="mt-3" />
+      Some of the services listed below may use Cookies to identify Users or
+      they may use the behavioral retargeting technique, i.e. displaying ads
+      tailored to the User’s interests and behavior, including those detected
+      outside this Website.
+      <h1 className="font-bold text-[1.5rem] my-8">
+        YOUR RIGHTS AND CONTROLS OVER THE DATA YOU PROVIDE
+      </h1>
+      We want you to stay in control of your personal data. Nextride provides
+      you with controls through the app where you can view your personal
+      information including your profile data and trip history. Hence, the
+      following are your rights over the information you’ve provided us with
+      <ul className="pl-10 my-12 list-decimal flex flex-col gap-6">
+        <li>
+          Your right of access: You can access your personal data via the
+          Nextride app. You have the right to ask us for copies of your personal
+          information. There are some exemptions, such as when we have to
+          balance the rights of others, which means you may not always receive
+          all the information we process.
+        </li>
+        <li>
+          Your right to rectification: You can access and update your personal
+          data via the Nextride app. You have the right to ask us to rectify
+          information you think is otherwise inaccurate. You also have the right
+          to ask us to complete information you think is incomplete.
+        </li>
+        <li>
+          Your right to erasure: You have the right to ask us to erase your
+          personal information in certain circumstances.
+        </li>
+        <li>
+          Your right to restriction of processing: You have the right to ask us
+          to restrict the processing of your information in certain
+          circumstances. This means that your data can only be used for certain
+          things, such as legal claims or to exercise legal rights.
+        </li>
+        <li>
+          Your right to object to processing: You may have the right to object
+          to processing if we are processing your information on the basis of
+          legitimate interests. You may submit an objection to any automated
+          decision we have made, and ask that a person reviews it.
+        </li>
+        <li>
+          Your right to data portability:You have the right to ask that we
+          transfer the information you gave us from one organisation to another,
+          or give it to you, in certain circumstances. This only applies to
+          information you have given us.
+        </li>
+      </ul>
+    </div>
+  );
+}
---
+++ app/terms/page.tsx
@@ -0,0 +1,101 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className="py-32 mb-40 max-width text-[1.1rem]">
+      
+      General Terms
+      <br />
+Once payment is made for Nextride hire services, it implies that you’re agreeing to all the terms and conditions stipulated, and compliance must be ensured. 
+<br />
+<br />
+
+Other Terms and Conditions for Hire
+<br />
+<ul className="list-decimal my-3 pb-20 pl-8 flex flex-col gap-1">
+  <li>
+The mode of payment shall be via bank transfer, and full payment will be only be accepted. 
+
+  </li>
+  <li>
+Hire hours is 15 hours.
+
+  </li>
+  <li>
+
+Only 60% of booking price refund will be made on cancellations within 72 hours notice.
+  </li>
+  <li>
+You will have to pay for every extra hour spent exceeding your 15 hours booking period. 
+
+  </li>
+  <li>
+In the case of extending beyond your agreed time, you will have to place the request and make payment before the expiration of that 15 hours. 
+
+  </li>
+  <li>
+Accommodation must be provided for drivers on inter-state travels. 
+
+  </li>
+  <li>
+You will be properly scrutinized before embarking on the trip. 
+
+  </li>
+  <li>
+Consumption of alcohol and smoking in the vehicle will not be allowed. 
+
+  </li>
+  <li>
+In the unlikely event that the vehicle is faulty, such complaint should be brought to the company’s notice within one hour of usage, and refund/change of vehicle will be duly made. 
+
+  </li>
+  <li>
+For Airport rides, Airport tolls and parking fees will be paid by the company.
+
+  </li>
+  <li>
+
+The driver is allowed to leave when the time is expired after notice of expiration by the company has been made to him. 
+  </li>
+  <li>
+    
+You must ensure that a good standard of cleanliness is kept within the vehicle, and instruments in the vehicle must not be tampered with, else you’ll be held responsible. 
+  </li>
+  <li>
+For bookings that are two days or longer, kindly note that the driver initially assigned to you may be replaced with another verified driver from our company.
+
+  </li>
+  <li>
+If peradventure you forget an item behind in our vehicle, you are expected to notify us in time. 
+    
+  </li>
+  <li>
+Our general pricing is within city rates. Outskirts places and interstate travels attracts extra charges. 
+
+  </li>
+</ul>
+
+
+
+
+
+
+
+Validity 
+<br/>
+All disputes arising from these terms and conditions shall be resolved by way of negotiations between the company and the hirer. If no resolution is reached, legal actions shall be initiated. 
+<br/>
+<br/>
+
+Termination 
+<br/>
+
+The company is entitled to terminate the agreement in cases of theft and bullying, or where it appears that the hirer is in breach of any of the terms and conditions. 
+<br/>
+<br/>
+<br/>
+<br/>
+
+    </div>
+  )
+}
