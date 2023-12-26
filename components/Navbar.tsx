cd52fc2370520fc2c04dfbf03e94df7d54f42881
muhammod1 <muhammodholawale@gmail.com>
2023-12-26 13:08:31 +0100

    contact us

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,12 @@
-"use client"
-import { useState } from 'react';
-import Link from 'next/link';
-import Image from 'next/image';
+"use client";
+import { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 const Navbar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const router = useRouter();
 
   const toggleMobileMenu = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
@@ -56,45 +58,89 @@ const Navbar = () => {
         {/* Mobile menu */}
         <div
           className={`${
-            isMobileMenuOpen ? 'block' : 'hidden'
+            isMobileMenuOpen ? "block" : "hidden"
           } md:hidden flex justify-start mt-14 items-center flex-col space-y-7 text-2xl bg-white p-4 absolute top-16 left-0 w-full h-screen`}
         >
-          <Link href="/" className="text-black hover:text-primary-blue w-full transition-all font-bold">
-          Become a Host
-          </Link>
-          <Link href="/about" className="text-black hover:text-primary-blue w-full transition-all font-bold">
-          About Us
-          </Link>
-          <Link href="#service" className="text-black hover:text-primary-blue w-full transition-all font-bold">
-          Our Services
-          </Link>
-          <Link href="#faq" className="text-black hover:text-primary-blue w-full transition-all font-bold">
-          FAQ
-          </Link>
-          <Link href="/contact" className="text-black hover:text-primary-blue w-full transition-all font-bold">
-          Contact Us
-          </Link>
+          <button
+            onClick={() => {
+              setIsMobileMenuOpen(!isMobileMenuOpen);
+              router.push("/");
+            }}
+            className="text-black text-left hover:text-primary-blue w-full transition-all font-bold"
+          >
+            Become a Host
+          </button>
+
+          <button
+            onClick={() => {
+              setIsMobileMenuOpen(!isMobileMenuOpen);
+              router.push("/about");
+            }}
+            className="text-black text-left hover:text-primary-blue w-full transition-all font-bold"
+          >
+            About Us
+          </button>
+          <button
+            onClick={() => {
+              setIsMobileMenuOpen(!isMobileMenuOpen);
+              router.push("/#service");
+            }}
+            className="text-black text-left hover:text-primary-blue w-full transition-all font-bold"
+          >
+            Our Services
+          </button>
+          <button
+            onClick={() => {
+              setIsMobileMenuOpen(!isMobileMenuOpen);
+              router.push("/#faq");
+            }}
+            className="text-black text-left hover:text-primary-blue w-full transition-all font-bold"
+          >
+            FAQ
+          </button>
+          <button
+            onClick={() => {
+              setIsMobileMenuOpen(!isMobileMenuOpen);
+              router.push("/contact");
+            }}
+            className="text-black text-left hover:text-primary-blue w-full transition-all font-bold"
+          >
+            Contact Us
+          </button>
         </div>
 
         {/* Desktop menu */}
         <div className="hidden md:flex gap-5">
-        <Link href="/" className="text-black hover:text-primary-blue font-bold">
-        Become a Host
+          <Link
+            href="/"
+            className="text-black hover:text-primary-blue font-bold"
+          >
+            Become a Host
           </Link>
-        <Link href="/about" className="text-black hover:text-primary-blue font-bold">
-        About us
-
+          <Link
+            href="/about"
+            className="text-black hover:text-primary-blue font-bold"
+          >
+            About us
           </Link>
-          <Link href="#service" className="text-black hover:text-primary-blue font-bold">
-        Our Services
+          <Link
+            href="#service"
+            className="text-black hover:text-primary-blue font-bold"
+          >
+            Our Services
           </Link>
-          <Link href="#faq" className="text-black hover:text-primary-blue font-bold">
-          FAQ
+          <Link
+            href="#faq"
+            className="text-black hover:text-primary-blue font-bold"
+          >
+            FAQ
           </Link>
-          <Link href="/contact" className="text-black hover:text-primary-blue font-bold">
-          Contact Us
+          <Link
+            href="/contact"
+            className="text-black hover:text-primary-blue font-bold"
+          >
+            Contact Us
           </Link>
-        
         </div>
       </nav>
     </header>
